c8ae47f424b7c175ce0b56f0941b47a309807307
windhunter9 <spotify-1984@web.de>
2018-12-10 19:21:44 +0100

    moved screenscraping files

--- windhunt/management/commands/script.py
+++ windhunt/management/commands/script.py
@@ -3,6 +3,9 @@ from django.core.management.base import BaseCommand
 from windhunt.models import Windforecast, WindMeasurement
 
 
+from ._screen_scraping import *
+from ._windfinder import *
+from ._station_lippesee import *
 
 class Command(BaseCommand):
     help = "<appropriate help text here>"
