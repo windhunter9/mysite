92ab4a916b4a89dff24e9ad6f3cb3bf8f31252db
windhunter9 <spotify-1984@web.de>
2019-09-04 20:06:54 +0200

    asdf

--- windhunt/models.py
+++ windhunt/models.py
@@ -4,50 +4,50 @@ import datetime
 # Create your models here.
 
 
-class Question(models.Model):
-    question_text = models.CharField(max_length=200)
-    pub_date = models.DateTimeField('date published')
-    def __str__(self):
-    	return self.question_text
-    def was_published_recently(self):
-    	now = timezone.now()
-    	return now - datetime.timedelta(days = 1) <= self.pub_date <= now
+# class Question(models.Model):
+#     question_text = models.CharField(max_length=200)
+#     pub_date = models.DateTimeField('date published')
+#     def __str__(self):
+#     	return self.question_text
+#     def was_published_recently(self):
+#     	now = timezone.now()
+#     	return now - datetime.timedelta(days = 1) <= self.pub_date <= now
 
-class Choice(models.Model):
-    question = models.ForeignKey(Question, on_delete=models.CASCADE)
-    choice_text = models.CharField(max_length=200)
-    votes = models.IntegerField(default=0)
-    def __str__(self):
-    	return self.choice_text
-
-
-class WindSuperforecast(models.Model):
-    id = models.AutoField(primary_key=True)
-    forecast_time = models.DateTimeField()
-    runtime = models.DateTimeField()
-    wind_max = models.FloatField()
-    wind_average = models.FloatField()
-    wind_angle = models.FloatField()
-    class Meta:
-        unique_together = (("forecast_time", "runtime"),)
+# class Choice(models.Model):
+#     question = models.ForeignKey(Question, on_delete=models.CASCADE)
+#     choice_text = models.CharField(max_length=200)
+#     votes = models.IntegerField(default=0)
+#     def __str__(self):
+#     	return self.choice_text
 
 
+# class WindSuperforecast(models.Model):
+#     id = models.AutoField(primary_key=True)
+#     forecast_time = models.DateTimeField()
+#     runtime = models.DateTimeField()
+#     wind_max = models.FloatField()
+#     wind_average = models.FloatField()
+#     wind_angle = models.FloatField()
+#     class Meta:
+#         unique_together = (("forecast_time", "runtime"),)
 
 
-class WindMeasurement(models.Model):
-    id = models.AutoField(primary_key=True)
-    runtime = models.DateTimeField(unique = True)
-    wind_max = models.FloatField()
-    wind_average = models.FloatField()
 
 
-class WindForecast(models.Model):
-    id = models.AutoField(primary_key=True)
-    forecast_time = models.DateTimeField( default=0)
-    runtime = models.DateTimeField()
-    wind_max = models.FloatField()
-    wind_average = models.FloatField()
-    wind_angle = models.FloatField()
-    class Meta:
-        unique_together = (("forecast_time", "runtime"),)
+# class WindMeasurement(models.Model):
+#     id = models.AutoField(primary_key=True)
+#     runtime = models.DateTimeField(unique = True)
+#     wind_max = models.FloatField()
+#     wind_average = models.FloatField()
+
+
+# class WindForecast(models.Model):
+#     id = models.AutoField(primary_key=True)
+#     forecast_time = models.DateTimeField( default=0)
+#     runtime = models.DateTimeField()
+#     wind_max = models.FloatField()
+#     wind_average = models.FloatField()
+#     wind_angle = models.FloatField()
+#     class Meta:
+#         unique_together = (("forecast_time", "runtime"),)
 
